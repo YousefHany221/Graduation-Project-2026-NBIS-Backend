074b657f9775603352e4fb6bd1a4e75712572c9b
YousefHany221 <146834220+YousefHany221@users.noreply.github.com>
2026-06-19 06:02:57 +0300

    Fix CORS: Enable Sanctum stateful middleware in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,6 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
@@ -39,13 +38,14 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // السطر التالي هو الأهم لحل مشكلة الـ CORS والـ Auth
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-            'setLanguage' => [
-                \App\Http\Middleware\SetLanguage::class,
-            ],
+        'setLanguage' => [
+            \App\Http\Middleware\SetLanguage::class,
+        ],
     ];
 
     /**
